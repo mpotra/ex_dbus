83acb9b27e94dc7f283f4d200a810d25c790e1e9
Mihai Potra <mike@mpotra.com>
2021-08-11 11:45:02 +0300

    Add find_field/2 in ErlangDBus.Message

--- lib/erlang_dbus/message.ex
+++ lib/erlang_dbus/message.ex
@@ -34,4 +34,41 @@ defmodule ErlangDBus.Message do
   def get_field(:signature, message) do
     :dbus_message.get_field(8, message)
   end
+
+  # Find field
+  def find_field(:invalid, message) do
+    :dbus_message.find_field(0, message)
+  end
+
+  def find_field(:path, message) do
+    :dbus_message.find_field(1, message)
+  end
+
+  def find_field(:interface, message) do
+    :dbus_message.find_field(2, message)
+  end
+
+  def find_field(:member, message) do
+    :dbus_message.find_field(3, message)
+  end
+
+  def find_field(:error_name, message) do
+    :dbus_message.find_field(4, message)
+  end
+
+  def find_field(:reply_serial, message) do
+    :dbus_message.find_field(5, message)
+  end
+
+  def find_field(:destination, message) do
+    :dbus_message.find_field(6, message)
+  end
+
+  def find_field(:sender, message) do
+    :dbus_message.find_field(7, message)
+  end
+
+  def find_field(:signature, message) do
+    :dbus_message.find_field(8, message)
+  end
 end
