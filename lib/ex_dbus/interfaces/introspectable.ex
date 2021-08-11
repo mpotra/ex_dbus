fd74ec35072b30630f82a8cd80f99959df67a4a6
Mihai Potra <mike@mpotra.com>
2021-08-11 11:48:43 +0300

    Fix DBus Introspectable interface

--- lib/ex_dbus/interfaces/introspectable.ex
+++ lib/ex_dbus/interfaces/introspectable.ex
@@ -1,26 +1,33 @@
 defmodule ExDBus.Interfaces.Introspectable do
   use ExDBus.Schema
-  alias ExDBus.Builder
 
   node do
     interface "org.freedesktop.DBus.Introspectable" do
       method "Introspect" do
         arg("xml_data", "s", :out)
-        callback(&__MODULE__.introspect/1)
+        callback(&__MODULE__.introspect/2)
       end
     end
   end
 
-  defp introspect({:object, _, _} = object) do
-    root =
-      Builder.root!()
-      |> Builder.Insert.insert(object)
+  def introspect(_, %{path: "/", node: {:object, _, children}}) do
+    children =
+      children
+      |> Enum.filter(&(elem(&1, 0) == :object))
+      |> Enum.map(fn {:object, name, _} -> {:object, name, []} end)
 
+    root = {:object, "", children}
+    xml_body = ExDBus.XML.to_xml(root, nested_objects: false)
+    {:ok, [:string], [xml_body]}
+  end
+
+  def introspect(_, %{node: {:object, _, children}}) do
+    root = {:object, "", children}
     xml_body = ExDBus.XML.to_xml(root)
     {:ok, [:string], [xml_body]}
   end
 
-  defp introspect(_) do
+  def introspect(_, _) do
     {:error, "org.freedesktop.DBus.Error.UnknownObject", "Object not valid"}
   end
 end
